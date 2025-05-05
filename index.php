e98c942597720e3f8a871a004930aa61107c63e6
Amdiathamidou <145067307+Amdiathamidou@users.noreply.github.com>
2025-05-05 17:51:58 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,307 @@
+
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> Plateforme de prise de rendez-vous </title>
+
+    <link rel="shortcut icon" href="assets/images/fav.jpg">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawsom-all.min.css">
+     <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
+
+    <body>
+
+    <!-- ################# Header Starts Here#######################--->
+    
+      <header id="menu-jk">
+    
+        <div id="nav-head" class="header-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3  col-sm-12" style="color:#000;font-weight:bold; font-size:42px; margin-top: 1% !important;">
+                    <a href="index.php">
+                        <img src="assets/images/images.png" alt="AMDI Fransta" style="width: 75px; height: auto;">
+                    </a>
+                       <a data-toggle="collapse" data-target="#menu" href="#menu" ><i class="fas d-block d-md-none small-menu fa-bars"></i></a>
+                    </div>
+                    <div id="menu" class="col-lg-8 col-md-9 d-none d-md-block nav-item">
+                        <ul>
+                        <li><a href="index.php">Accueil</a></li>
+                            
+                            <li><a href="#about_us">A propos de nous</a></li>
+                            <li><a href="rdv/connexion.php">Connexion</a></li>
+                            <li><a href="#services">Services</a></li>
+                            
+                        </ul>
+                    </div>
+                    <div class="col-sm-2 d-none d-lg-block appoint">
+                        <a class="btn btn-success" href="rdv/connexion.php">Prende RDV</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </header>
+    
+     <!-- ################# Slider Starts Here#######################--->
+
+    <div class="slider-detail">
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            </ol>
+
+
+   
+
+
+            <div class="carousel-inner">
+                <div class="carousel-item ">
+                    <img class="d-block w-100" src="assets/images/slider/slider_2.jpg" alt="Second slide">
+                    <div class="carousel-cover"></div>
+                    <div class="carousel-caption vdg-cur d-none d-md-block">
+                        <h5 class="animated bounceInDown">Plateforme de prise de rendez-vous</h5>
+            
+                         
+                    
+                    </div>
+                </div>
+                
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="assets/images/slider/slider_3.jpg" alt="Third slide">
+                      <div class="carousel-cover"></div>
+                    <div class="carousel-caption vdg-cur d-none d-md-block">
+                        <h5 class="animated bounceInDown">Plateforme de prise de rendez-vous</h5>
+            
+                         
+                    
+                    </div>
+              
+                </div>
+                
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+
+    </div>
+    
+  <!--  ************************* connexion ************************** -->
+    
+    
+    
+
+
+
+
+
+
+
+    <!-- ################# Our Departments Starts Here#######################--->
+
+
+    <section id="services" class="key-features department">
+        <div class="container">
+            <div class="inner-title">
+
+                <h2>Nos principales caractéristiques</h2>
+                <p>Jetez un œil à certaines de nos principales caractéristiques</p>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-heartbeat"></i>
+                        <h5>Cardiologie</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-ribbon"></i>
+                        <h5>Orthopédique</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                       <i class="fab fa-monero"></i>
+                        <h5>Neurologue</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-capsules"></i>
+                        <h5>medicament</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-prescription-bottle-alt"></i>
+                        <h5>psychologue</h5>
+                    </div>
+                </div>
+
+
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="far fa-thumbs-up"></i>
+                        <h5>Traitements de haute qualité</h5>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+        </div>
+
+    </section>
+    
+    
+  
+    
+    <!--  ************************* About Us Starts Here ************************** -->
+        
+    <section id="about_us" class="about-us">
+        <div class="row no-margin">
+            <div class="col-sm-6 image-bg no-padding">
+                
+            </div>
+            <div class="col-sm-6 abut-yoiu">
+                <h3>A propos de notre plateform</h3>
+             
+            </div>
+        </div>
+    </section>    
+    
+    
+            <!--  ************************* Gallery Starts Here ************************** -->
+        <div id="gallery" class="gallery">    
+           <div class="container">
+              <div class="inner-title">
+
+                <h2>Notre galerie</h2>
+                <p>Voir notre galerie</p>
+            </div>
+              <div class="row">
+                
+
+        <div class="gallery-filter d-none d-sm-block">
+            <button class="btn btn-default filter-button" data-filter="all">Tous</button>
+            <button class="btn btn-default filter-button" data-filter="hdpe">Dentaire</button>
+            <button class="btn btn-default filter-button" data-filter="sprinkle">Cardiologie</button>
+            <button class="btn btn-default filter-button" data-filter="spray"> Neurologie</button>
+            <button class="btn btn-default filter-button" data-filter="irrigation">Laboratoire</button>
+        </div>
+        <br/>
+
+
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="assets/images/gallery/gallery_01.jpg" class="img-responsive">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter sprinkle">
+                <img src="assets/images/gallery/gallery_02.jpg" class="img-responsive">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="assets/images/gallery/gallery_03.jpg" class="img-responsive">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter irrigation">
+                <img src="assets/images/gallery/gallery_04.jpg" class="img-responsive">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter spray">
+                <img src="assets/images/gallery/gallery_05.jpg" class="img-responsive">
+            </div>
+
+          
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter spray">
+                <img src="assets/images/gallery/gallery_06.jpg" class="img-responsive">
+            </div>
+
+        </div>
+    </div>
+       
+       
+       </div>
+        <!-- ######## Gallery End ####### -->
+    
+    
+    
+    
+    <!-- ################# Footer Starts Here#######################--->
+
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+       
+                <div class="col-md-6 col-sm-12">
+                    <h2>Liens Utils</h2>
+                    <ul class="list-unstyled link-list">
+                        <li><a ui-sref="about" href="#about">A propos de Nous</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="portfolio" href="#services">Services</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="products" href="#logins">Connexion</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="gallery" href="#gallery">Gallerie</a><i class="fa fa-angle-right"></i></li>
+                        
+                    </ul>
+                </div>
+                <div class="col-md-6 col-sm-12 map-img">
+                    <h2>Nous contacter</h2>
+
+                    <address class="md-margin-bottom-40">
+                </div>
+            </div>
+        </div>
+        
+
+    </footer>
+    <div class="copy">
+            <div class="container">
+         Plateforme de prise de rendez-vous 
+                
+     
+            </div>
+
+        </div>
+    
+    </body>
+
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/plugins/scroll-nav/js/jquery.easing.min.js"></script>
+<script src="assets/plugins/scroll-nav/js/scrolling-nav.js"></script>
+<script src="assets/plugins/scroll-fixed/jquery-scrolltofixed-min.js"></script>
+
+<script src="assets/js/script.js"></script>
+
+
+
+</html>
